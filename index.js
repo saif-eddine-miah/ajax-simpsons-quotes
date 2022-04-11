de0cf1e008cdd14f594fb2dfe149825ea2d1cd96
saif-eddine-miah <saifeddinemiah@gmail.com>
2022-04-11 11:41:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function fetchSimpsonJSON() {
+    const url = `https://simpsons--api.herokuapp.com/quotes`;
+    axios.get(url)
+      .then(function(response) {
+        return response.data; 
+      })
+      .then(function(simpson) {
+        console.log('data decoded from JSON:', simpson);
+
+        const simpsonHtml = `
+          <p><strong>${simpson[0].character}</strong></p>
+          <p><strong>${simpson[0].quote}</strong></p>
+          <img src="${simpson[0].image}" />
+        `;
+        document.querySelector('#simpson').innerHTML = simpsonHtml;
+      });
+  }
+  
+  fetchSimpsonJSON();
